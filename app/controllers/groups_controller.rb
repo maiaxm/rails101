e4aab86263b7d24c230dd640460cfa79695da023
maia <6531921@qq.com>
2017-04-03 15:11:15 +0800

    implement my groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-
+  before_action :authenticate_user!
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destory, :join , :quit]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
